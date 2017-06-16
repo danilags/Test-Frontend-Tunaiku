2417607d2a645c30dd7ac95345f980cb558b1b57
danilags <aguspermadi94@gmail.com>
2017-06-16 23:22:25 +0700

    adding row 3 half done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import logo from '../public/logo.png';
 import iconSearch from '../public/icon-search.png';
 import section1 from '../public/section-1-image01.png';
 
+import section1a from '../public/section-2-image01.png';
+import section1b from '../public/section-2-image02.png';
+import section1c from '../public/section-2-image03.png';
+
 import { Container, Row, Col } from 'react-grid-system';
 
 class App extends Component {
@@ -48,6 +52,54 @@ class App extends Component {
               <Col md={6}>
                 <div>
                   <img src={section1} alt="money" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+
+        <div className="baris-ketiga">
+          <Container>
+            <Row className="center-text">
+              <Col md={12}>
+                <h1>Cupidatat Non Proident Sunt</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={4}>
+                <div className="image-icon">
+                  <img src={section1a} alt="koin" />
+                </div>
+                <div className="description">
+                  <h2>Cupidatat Non Proident Sunt</h2>
+                  <p>laboris nisi utaliquip ex ea commodo consequat
+                    irure dolor in reprehenderit in voluptate
+                    dolore eu fugiat nulla pariatur
+                  </p>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className="image-icon">
+                  <img src={section1b} alt="koin" />
+                </div>
+                <div className="description">
+                  <h2>Cupidatat Non Proident Sunt</h2>
+                  <p>laboris nisi utaliquip ex ea commodo consequat
+                    irure dolor in reprehenderit in voluptate
+                    dolore eu fugiat nulla pariatur
+                  </p>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className="image-icon">
+                  <img src={section1c} alt="koin" />
+                </div>
+                <div className="description">
+                  <h2>Cupidatat Non Proident Sunt</h2>
+                  <p>laboris nisi utaliquip ex ea commodo consequat
+                    irure dolor in reprehenderit in voluptate
+                    dolore eu fugiat nulla pariatur
+                  </p>
                 </div>
               </Col>
             </Row>
